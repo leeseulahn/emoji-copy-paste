b2aa5588693ae2863c484dcdf5f691e9bf811df3
leeseulahn <118793632+leeseulahn@users.noreply.github.com>
2026-03-19 14:30:19 +0900

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://emoji-copy-paste-jtse.vercel.app/sitemap.xml",
+  };
+}
