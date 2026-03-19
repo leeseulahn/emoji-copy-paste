281e42a60fb39beee46054d125edbb4afcebcd9e
leeseulahn <118793632+leeseulahn@users.noreply.github.com>
2026-03-19 14:30:10 +0900

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://emoji-copy-paste-jtse.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://emoji-copy-paste-jtse.vercel.app/emoji-combiner",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://emoji-copy-paste-jtse.vercel.app/emoji-kitchen",
+      lastModified: new Date(),
+    },
+  ];
+}
